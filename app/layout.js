e9e4c229628ebcec4ad4911104072d187fc1172d
RootsRaddik6 <korjamltd@gmail.com>
2025-11-28 10:39:39 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,8 +9,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+   <html lang="en">
+  <head>
+    <meta name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
+  </head>
+  <body>{children}</body>
+</html>
   );
 }
