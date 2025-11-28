930a092f7b5e6c5d81663da4ed379088dcf46f7c
RootsRaddik6 <korjamltd@gmail.com>
2025-11-28 10:37:43 -0500

    Update loader.js

--- app/sbt-gateway/loader.js
+++ app/sbt-gateway/loader.js
@@ -2,13 +2,24 @@
 
 import Detector from "./detector";
 import Gatekeeper from "./gatekeeper";
+import { preflight } from "./preflight";
 
 export default async function loadSbtGateway(onAuthorized) {
+  const ok = await preflight();
+  if (!ok) return;
+
   const detector = new Detector();
+
   detector.onHasSbt = () => {
     new Gatekeeper().openPortal();
+    sessionStorage.setItem("sbtVerified", "true");
     onAuthorized && onAuthorized();
   };
-  detector.onNoSbt = () => console.log("No soul SBT found.");
+
+  detector.onNoSbt = () => {
+    sessionStorage.setItem("sbtVerified", "false");
+    console.log("No SBT soul detected.");
+  };
+
   await detector.init();
 }
