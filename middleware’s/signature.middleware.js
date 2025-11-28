c233db27b5547772eab362286c97ba82c9a1076c
RootsRaddik6 <korjamltd@gmail.com>
2025-11-28 14:34:04 -0500

    Create signature.middleware.js

---
+++ middleware’s/signature.middleware.js
@@ -0,0 +1,7 @@
+export async function verifySignature({ address, signature, message }) {
+  // TODO: implement TON/Tezos signature verification
+  // For PoC return true; replace with real verification using libs:
+  // - Tezos: @taquito/utils / @airgap/beacon-sdk verify
+  // - TON: tonweb / ton-client-js verify
+  return true;
+}
