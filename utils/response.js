d576e6b717e2ba438edd1d4595c526aa35a91716
RootsRaddik6 <korjamltd@gmail.com>
2025-11-28 13:13:08 -0500

    Update response.js

--- utils/response.js
+++ utils/response.js
@@ -1,1 +1,12 @@
-
+export function jsonOK(data = {}, status = 200) {
+  return new Response(JSON.stringify({ ok: true, data }), {
+    status,
+    headers: { "Content-Type": "application/json" }
+  });
+}
+export function jsonErr(message = "Error", status = 500, extra = {}) {
+  return new Response(JSON.stringify({ ok: false, error: message, ...extra }), {
+    status,
+    headers: { "Content-Type": "application/json" }
+  });
+}
